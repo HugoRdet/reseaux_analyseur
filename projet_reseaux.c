769ad76996a90356b41f56a923ac5aa57faa6dab
Hugo Riffaud de Turckheim <hugordet@gmail.com>
2020-10-31 20:42:39 +0100

    ajout couche TCP
    -début du traitement de la couche TCP (jusqu'a l'acknowledgement number)

--- projet_reseaux.c
+++ projet_reseaux.c
@@ -255,9 +255,9 @@ int lecture_trame(FILE *fichier_source,FILE *fichier_dest,int nb_trame){
 	fprintf(fichier_dest,"\n");
 		
 	
-	fprintf(fichier_dest,"couche ethernet:\n");
-	fprintf(fichier_dest,"----------------\n");
-	fprintf(fichier_dest,"\tsource :");
+	fprintf(fichier_dest,"\tcouche ethernet:\n");
+	fprintf(fichier_dest,"\t----------------\n");
+	fprintf(fichier_dest,"\t\tsource :");
 	fprintf(fichier_dest," %.2x:",tab_ligne[6]);
 	fprintf(fichier_dest," %.2x:",tab_ligne[7]);
 	fprintf(fichier_dest," %.2x:",tab_ligne[8]);
@@ -265,7 +265,7 @@ int lecture_trame(FILE *fichier_source,FILE *fichier_dest,int nb_trame){
 	fprintf(fichier_dest," %.2x:",tab_ligne[10]);
 	fprintf(fichier_dest," %.2x\n",tab_ligne[11]);
 	
-	fprintf(fichier_dest,"\tdestination :");
+	fprintf(fichier_dest,"\t\tdestination :");
 	fprintf(fichier_dest," %.2x:",tab_ligne[0]);
 	fprintf(fichier_dest," %.2x:",tab_ligne[1]);
 	fprintf(fichier_dest," %.2x:",tab_ligne[2]);
@@ -273,58 +273,58 @@ int lecture_trame(FILE *fichier_source,FILE *fichier_dest,int nb_trame){
 	fprintf(fichier_dest," %.2x:",tab_ligne[4]);
 	fprintf(fichier_dest," %.2x\n",tab_ligne[5]);
 	
-	fprintf(fichier_dest,"\ttype: 0x%.2x%.2x",tab_ligne[12],tab_ligne[13]);
+	fprintf(fichier_dest,"\t\ttype: 0x%.2x%.2x",tab_ligne[12],tab_ligne[13]);
 	if ((tab_ligne[12]==8)&&(tab_ligne[13]==0)){
 		fprintf(fichier_dest," (IPv4)\n");
 	}
 	fprintf(fichier_dest,"\n");
 	//fin de la couche ethernet , j'ai pas fait une fonction 
 	//special couche ethernet car on est pas arrivés à la fin de la ligne.		
-	fprintf(fichier_dest,"couche IP :\n");
-	fprintf(fichier_dest,"-----------\n");
-	fprintf(fichier_dest,"\tVersion : %d\n",val_byte_1(tab_ligne[14]));
+	fprintf(fichier_dest,"\tcouche IP :\n");
+	fprintf(fichier_dest,"\t-----------\n");
+	fprintf(fichier_dest,"\t\tVersion : %d\n",val_byte_1(tab_ligne[14]));
 	int IHL=val_byte_0(tab_ligne[14]);
 	//le IHL est en quad words ici.
 	unsigned int len_IHL=IHL*4;
-	fprintf(fichier_dest,"\tLongueur header (IHL) : %d (%d)\n",len_IHL,IHL);
-	fprintf(fichier_dest,"\tDifferentiated Services Codepoint: %d\n",val_byte_1(tab_ligne[15]));
-	fprintf(fichier_dest,"\tExplicit Congestion Notification: %d\n",val_byte_0(tab_ligne[15]));
+	fprintf(fichier_dest,"\t\tLongueur header (IHL) : %d (%d)\n",len_IHL,IHL);
+	fprintf(fichier_dest,"\t\tDifferentiated Services Codepoint: %d\n",val_byte_1(tab_ligne[15]));
+	fprintf(fichier_dest,"\t\tExplicit Congestion Notification: %d\n",val_byte_0(tab_ligne[15]));
 	//ligne 2;
 	
 	//chargement de la seconde ligne.
 	verif=cherche_prochaine_ligne(fichier_source,1,tab_ligne,nb_trame);
 	if (!verif){
-		printf("trame %d incomplète \n",nb_trame);
+		printf("\ttrame %d incomplète \n",nb_trame);
 		return 0;
 	}
 	
 	unsigned int longueur_totale=tab_ligne[0]*256+tab_ligne[1];
-	fprintf(fichier_dest,"\tlongueur totale: %d\n",longueur_totale );
-	fprintf(fichier_dest,"\tIdentification; 0x%.2x%.2x (%d)\n",tab_ligne[2],tab_ligne[3],tab_ligne[2]*256+tab_ligne[3]);
-	fprintf(fichier_dest,"\tflags :0x%.2x%.2x\n",tab_ligne[4],tab_ligne[5]);
+	fprintf(fichier_dest,"\t\tlongueur totale: %d\n",longueur_totale );
+	fprintf(fichier_dest,"\t\tIdentification; 0x%.2x%.2x (%d)\n",tab_ligne[2],tab_ligne[3],tab_ligne[2]*256+tab_ligne[3]);
+	fprintf(fichier_dest,"\t\tflags :0x%.2x%.2x\n",tab_ligne[4],tab_ligne[5]);
 		
-	fprintf(fichier_dest,"\t\t(R)  Reserved bit  : \t%d\n",valeur_n_eme_bit(tab_ligne[4],7));
-	fprintf(fichier_dest,"\t\t(DF) Don't Fragment: \t%d\n",valeur_n_eme_bit(tab_ligne[4],6));
-	fprintf(fichier_dest,"\t\t(MF) More Fragments: \t%d\n",valeur_n_eme_bit(tab_ligne[4],5));
+	fprintf(fichier_dest,"\t\t\t(R)  Reserved bit  : \t%d\n",valeur_n_eme_bit(tab_ligne[4],7));
+	fprintf(fichier_dest,"\t\t\t(DF) Don't Fragment: \t%d\n",valeur_n_eme_bit(tab_ligne[4],6));
+	fprintf(fichier_dest,"\t\t\t(MF) More Fragments: \t%d\n",valeur_n_eme_bit(tab_ligne[4],5));
 	//j'ai préféré ne pas faire de boucle ici car le nombre d'iterations est fixe
 	int Frag_offset=valeur_n_eme_bit(tab_ligne[4],4)*((int) pow(16,12))+
 	valeur_n_eme_bit(tab_ligne[4],3)*((int) pow(16,11))+
 	valeur_n_eme_bit(tab_ligne[4],2)*((int) pow(16,10))+
 	valeur_n_eme_bit(tab_ligne[4],1)*((int) pow(16,9))+
 	valeur_n_eme_bit(tab_ligne[4],0)*((int) pow(16,8))+tab_ligne[5];
-	fprintf(fichier_dest,"\tFragment offset:\t%d\n",Frag_offset);
+	fprintf(fichier_dest,"\t\tFragment offset:\t%d\n",Frag_offset);
 	
-	fprintf(fichier_dest,"\tTime To Live:\t%d\n",tab_ligne[6]);
-	fprintf(fichier_dest,"\tProtocol:\t%d ",tab_ligne[7]);
-	fprintf(fichier_dest,"\tHeader Checksum :0x%.2x%.2x\n",tab_ligne[8],tab_ligne[9]);
-	fprintf(fichier_dest,"\tsource :");
+	fprintf(fichier_dest,"\t\tTime To Live:\t%d\n",tab_ligne[6]);
+	fprintf(fichier_dest,"\t\tProtocol:\t%d ",tab_ligne[7]);
+	fprintf(fichier_dest,"\t\tHeader Checksum :0x%.2x%.2x\n",tab_ligne[8],tab_ligne[9]);
+	fprintf(fichier_dest,"\t\tsource :");
 		fprintf(fichier_dest," %.d.",tab_ligne[10]);
 		fprintf(fichier_dest," %.d.",tab_ligne[11]);
 		fprintf(fichier_dest," %.d.",tab_ligne[12]);
 		fprintf(fichier_dest," %.d\n",tab_ligne[13]);
 	
 	
-	fprintf(fichier_dest,"\tsource :");
+	fprintf(fichier_dest,"\t\tsource :");
 	fprintf(fichier_dest," %.d:",tab_ligne[14]);
 	fprintf(fichier_dest," %.d:",tab_ligne[15]);
 	
@@ -335,10 +335,25 @@ int lecture_trame(FILE *fichier_source,FILE *fichier_dest,int nb_trame){
 	}	
 	fprintf(fichier_dest," %.d:",tab_ligne[0]);
 	fprintf(fichier_dest," %.d\n",tab_ligne[1]);
-	
 	//fin de la couche IP
-	
+		
 	//LES OPTIONS NE SONT PAS IMPLÉMENTÉES
+	fprintf(fichier_dest,"\n\tCOUCHE TCP :\n");
+	fprintf(fichier_dest,"\t------------\n");
+	fprintf(fichier_dest,"\t\tSource Port: %d\n",(tab_ligne[2])*256+tab_ligne[3]);
+	fprintf(fichier_dest,"\t\tDestination Port: %d\n",(tab_ligne[4])*256+tab_ligne[5]);
+	
+	
+	long int sequence_number=(tab_ligne[6])*((long int)pow(16,6))
+						+(tab_ligne[7])*((int)pow(16,4))
+						+(tab_ligne[8])*((int)pow(16,2))
+						+(tab_ligne[9]);	
+	fprintf(fichier_dest,"\t\tSequence number (raw) :%li \n",sequence_number);
+	long int acknowledgment_number=(tab_ligne[10])*((long int)pow(16,6))
+							+(tab_ligne[11])*((int)pow(16,4))
+							+(tab_ligne[12])*((int)pow(16,2))
+							+(tab_ligne[13]);
+	fprintf(fichier_dest,"\t\tacknowledgement number (raw) :%li \n",acknowledgment_number);
 	return 1;
 	
 }
