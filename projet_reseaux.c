81d4606efc4a4ca4ad6b5727c4fcf8b45ec0279b
Hugo Riffaud de Turckheim <hugordet@gmail.com>
2020-11-05 22:52:26 +0100

    tests

--- projet_reseaux.c
+++ projet_reseaux.c
@@ -20,17 +20,42 @@ int main(int    argc, char **argv) {
 	printf("trames scanees : %d\n",cpt);
 	fclose(fichier_dest);
 	
-
 	
 	
-	GtkApplication *app;
-	int status;
-	app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv);
-	g_object_unref (app);
+	
+	
+	GtkBuilder *builder;
+	GObject *window;
+	GObject *button;
+	GError *error = NULL;
 
-	return status;
+	gtk_init (&argc, &argv);
+
+	/* Construct a GtkBuilder instance and load our UI description */
+	builder = gtk_builder_new ();
+	if (gtk_builder_add_from_file (builder, "builder.ui", &error) == 0)
+		{
+			g_printerr ("Error loading file: %s\n", error->message);
+			g_clear_error (&error);
+			return 1;
+		}
+
+	/* Connect signal handlers to the constructed widgets. */
+	window = gtk_builder_get_object (builder, "window");
+	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+	button = gtk_builder_get_object (builder, "button1");
+	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+	button = gtk_builder_get_object (builder, "button2");
+	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+	button = gtk_builder_get_object (builder, "quit");
+	g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+
+	gtk_main ();
+
+	return 0;
 	
 	
 	
